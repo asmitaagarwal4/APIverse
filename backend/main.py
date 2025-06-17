13f64603a0ed5586f53a7071f1132497630e5449
Asmita <118665910+asmitaagarwal4@users.noreply.github.com>
2025-06-17 17:13:37 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -31,4 +31,6 @@ def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
