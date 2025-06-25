fc20fd77cc881b3204bc0f97feb2fe62ca51aab9
asmitaagarwal4 <118665910+asmitaagarwal4@users.noreply.github.com>
2025-06-25 20:50:31 +0530

    refactor: database connection code in db.py

--- backend/db.py
+++ backend/db.py
@@ -1,10 +1,10 @@
-import os
-from pymongo import MongoClient
-from dotenv import load_dotenv
-load_dotenv()
+# import os
+# from pymongo import MongoClient
+# from dotenv import load_dotenv
+# load_dotenv()
 
-MONGO_URI = os.getenv("MONGO_URI")
-client = MongoClient(MONGO_URI)
-db = client["apiverse"]
-collection = db["apis"]
-reviews = db["reviews"]
+# MONGO_URI = os.getenv("MONGO_URI")
+# client = MongoClient(MONGO_URI)
+# db = client["apiverse"]
+# collection = db["apis"]
+# reviews = db["reviews"]
